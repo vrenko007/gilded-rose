d45c20ab5b19694e70957d1ec80c4a76af79f5c7
Matic Vrenko <matic.vrenko@dlabs.si>
2020-05-12 16:02:44 +0200

    Refactor Update

--- index.ts
+++ index.ts
@@ -5,54 +5,48 @@ interface Item {
 }
 
 export function updateQuality(items: Item[] = []) {
-    for (var i = 0; i < items.length; i++) {
-        if (
-            items[i].name != "Aged Brie" &&
-            items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-        ) {
-            if (items[i].quality > 0) {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                    items[i].quality = items[i].quality - 1;
-                }
-            }
-        } else {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                    if (items[i].sellIn < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                    if (items[i].sellIn < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
+    items.forEach((item) => {
+        //Sulfuras never changes
+        if (item.name == 'Sulfuras, Hand of Ragnaros') {
+            return;
         }
-        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
-        if (items[i].sellIn < 0) {
-            if (items[i].name != "Aged Brie") {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if (items[i].quality > 0) {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                } else {
-                    items[i].quality = items[i].quality - items[i].quality;
+
+        switch (item.name) {
+            case 'Backstage passes to a TAFKAL80ETC concert':
+                if (item.sellIn < 11) {
+                    item.quality += 1;
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.sellIn < 6) {
+                    item.quality += 1;
                 }
-            }
+                item.quality += 1;
+                if (item.sellIn < 1) {
+                    item.quality = 0;
+                }
+                break;
+            /* you could do passthrough without break then lines 23 to 25 become:
+                if (item.sellIn < 1) {
+                    item.quality = -1;
+                }
+            */
+            case 'Aged Brie':
+                item.quality += 1;
+                break;
+            case 'Conjured':
+                item.quality -= item.sellIn < 0 ? 4 : 2;
+                break;
+            /* you could do passthrough without break then line 36 becomes:
+                item.quality -= item.sellIn < 0 ? 2 : 1;
+            */
+            default:
+                item.quality -= item.sellIn < 0 ? 2 : 1;
+                break;
         }
-    }
+
+        item.sellIn -= 1;
+        item.quality = item.quality < 0 ? 0 : item.quality;
+        item.quality = item.quality > 50 ? 50 : item.quality;
+    });
 
     return items;
 }
